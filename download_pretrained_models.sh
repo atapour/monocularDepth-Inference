2998ef863ceae8a0f85498e08249875f79b2a65a
atapour <15729011+atapour@users.noreply.github.com>
2018-05-25 10:22:06 +0100

    models are downloaded from collections now

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -1,39 +1,25 @@
 echo "downloading pretrained models..."
 
 mkdir -p ./checkpoints
-MODEL_A2B=./checkpoints/netG_A2B.pth
-URL_A2B=https://www.dropbox.com/s/zg2txeh8u2crspd/netG_A2B.pth
-
-MODEL_B2C=./checkpoints/netG_B2C.pth
-URL_B2C=https://www.dropbox.com/s/jzo0zf0dp17x6ig/netG_B2C.pth
 
 MODELS=./checkpoints/checkpoints.zip
-URL_MODELS=https://collections.durham.ac.uk/single_use_link/show/b83f95631a515561056ee69445f9c94003b1e1986df584caed035263d610e3f6
-
-echo "downloading the style transfer model"
-
-wget --quiet --show-progress $URL_A2B -O $MODEL_A2B
-
-echo "downloading the depth estimation model"
-
-wget --quiet --show-progress $URL_B2C -O $MODEL_B2C
+URL_MODELS=https://collections.durham.ac.uk/downloads/r2rf55z770q
 
 echo "downloading the style transfer and depth estimation models..."
 
 wget --quiet --show-progress $URL_MODELS -O $MODELS
 
-echo "checking MD5 checksum for downloaded models..."
+echo "checking the MD5 checksum for downloaded models..."
 
 cd checkpoints
 
-CHECK_SUM_A2B='988541ba8fb6c355cac40a0099f81adc  netG_A2B.pth'
-CHECK_SUM_B2C='ac637db112f358566f071e2bbfd57675  netG_B2C.pth'
-CHECK_SUM_CHECKPOINTS='ea88bd783fc5b25a0a50fc18d25b2a3e  checkpoints.zip'
+CHECK_SUM_CHECKPOINTS='b176b00450ce9aaf3ef812087ed3ef49  checkpoints.zip'
 
-echo $CHECK_SUM_A2B | md5sum -c
-echo $CHECK_SUM_B2C | md5sum -c
 echo $CHECK_SUM_CHECKPOINTS | md5sum -c
 
 echo "Unpacking the zip file..."
 
-unzip checkpoints.zip && rm checkpoints.zip
+unzip -q checkpoints.zip && rm checkpoints.zip && rm README.txt
+
+echo "All Done!!"
+
